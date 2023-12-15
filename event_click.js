fdf0f429e820e7dca9ca61d01aac71841e37549f
shoichiro-u <136064799+shoichiro-u@users.noreply.github.com>
2023-12-15 14:40:15 +0900

    Add files via upload

---
+++ event_click.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+//デフォルトはモーダルウィンドウを閉じる
+    show: false,
+    path: '/Users/shoichiro-u/Desktop/Vue-dustbox/dustbox.gif'
+    }
+  },
+  methods:{
+//モーダルウィンドウを開く要素をクリックしたら
+    open: function(){
+      this.show = true
+      this.path = '/Users/shoichiro-u/Desktop/Vue-dustbox/graph.gif';
+    },
+//モーダルウィンドウを閉じる要素をクリックしたら
+    close: function(){
+      this.show = false
+      this.path = '/Users/shoichiro-u/Desktop/Vue-dustbox/dustbox.gif';
+    },
+  }
+});
+app.mount("#app");
