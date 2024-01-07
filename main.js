05e2bd0b1975f1eb00370b4f3aa128e1921570a1
takezakisibuki <kaizyudebugon@gmail.com>
2024-01-07 22:52:55 +0900

    chenge

--- main.js
+++ main.js
@@ -285,6 +285,18 @@ val.then(result => {
 }).catch(error => {
     console.error('Errorghjkl;:', error);
 });
+// reloadの基本的な使い方
+function doReload() {
+
+    // reloadメソッドによりページをリロード
+    window.location.reload();
+}
+ 
+window.addEventListener('load', function () {
+
+    // ページ表示完了した5秒後にリロード
+    setTimeout(doReload, 5000);
+});
 
 
 // var pieData = [
