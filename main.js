04c05f2b79b319435296ece34074eca825a7f316
takezakisibuki <kaizyudebugon@gmail.com>
2024-01-13 16:58:25 +0900

    chenge

--- main.js
+++ main.js
@@ -110,146 +110,6 @@ async function fetchCountTodos() {
     }
 }
 
-// MutationButton.addEventListener('click', (evt) => {
-//     addTodo().then((evt) => {
-//         MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
-//     });
-// });
-// async function fetchCountTodos() {
-//     try {
-//         var values=[];
-//         for(var i=0;i<5;i++){
-//             const response = await client.graphql({
-//                 query: listTodos
-//                 // variables: {
-//                 //     filter: {name:{ eq: 'Use AppSync' }},
-//                 //     limit: null, // 取得するアイテムの数を制限する場合
-//                 //     nextToken: null // ページネーションのためのトークンなど
-//                 //   }
-
-//             });
-//             const items = response.data.listTodos.items;
-//             alert(items.length);
-//             values[i]=items.length;
-//         }
-//             // const input ={
-//             //     name: { eq: 'Use AppSync' }
-//             // }
-//             // const response = await client.graphql({
-//             //     query: listTodos,
-//             //     variables: {
-//             //         filter: input,
-//             //         limit: null, // 取得するアイテムの数を制限する場合
-//             //         nextToken: null // ページネーションのためのトークンなど
-//             //     }
-//             // });
-        
-//             // const items = response.data.listTodos.items;
-//             // alert(items.length);
-//             // values[i]=[items.length];
-//             // response.data.listTodos.items.map((todo, i) => {
-//             //     QueryResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`;
-//             // });
-        
-//         return values;
-        
-//     } catch (e) {
-//         console.log('Something went wrong', e);
-//     }
-// }
-
-// const val = fetchCountTodos();
-// val.then(result => {
-//     console.log(result)
-//     var pieData = [
-//         {
-//             value: result[0],            // 値
-//             color:"#F7464A",       // 色
-//             highlight: "#FF5A5E",  // マウスが載った際の色
-//             label: "ペットボトル"        // ラベル
-//         },
-//         {
-//             value: result[1],
-//             color: "#41C44E",
-//             highlight: "#6CD173",
-//             label: "カン"
-//         },
-//         {
-//             value: result[2],
-//             color: "#FDB45C",
-//             highlight: "#FFC870",
-//             label: "ビン"
-//         },
-//         {
-//             value: result[3],
-//             color: "#AA49B8",
-//             highlight: "#C583CF",
-//             label: "可燃ごみ"
-//         },
-//         {
-//             value: result[4],
-//             color: "#4D5360",
-//             highlight: "#616774",
-//             label: "その他"
-//         }
-        
-//     ];
-//     console.log(pieData);
-//     function DrawPieChart() {
-//     var ctx = document.getElementById("graph-area").getContext("2d");
-//     window.myPie = new Chart(ctx).Pie(pieData);
-//     };      
-//     DrawPieChart();
-    
-// }).catch(error => {
-//     console.error('Error:', error);
-// });
-
-// PrintButton.addEventListener('click', (evt) => {
- 
-//     // ▼グラフの中身
-//     var pieData = [
-//        {
-//           value: 240,            // 値
-//           color:"#F7464A",       // 色
-//           highlight: "#FF5A5E",  // マウスが載った際の色
-//           label: "りんご"        // ラベル
-//        },
-//        {
-//           value: 50,
-//           color: "#41C44E",
-//           highlight: "#6CD173",
-//           label: "メロン"
-//        },
-//        {
-//           value: 100,
-//           color: "#FDB45C",
-//           highlight: "#FFC870",
-//           label: "みかん"
-//        },
-//        {
-//           value: 65,
-//           color: "#AA49B8",
-//           highlight: "#C583CF",
-//           label: "ぶどう"
-//        },
-//        {
-//           value: 75,
-//           color: "#4D5360",
-//           highlight: "#616774",
-//           label: "その他"
-//        }
- 
-//     ];
- 
-//     // ▼上記のグラフを描画するための記述
-//     window.onload = function(){
-//        var ctx = document.getElementById("graph-area").getContext("2d");
-//        window.myPie = new Chart(ctx).Pie(pieData);
-//     };
- 
-
-// });
 
 function subscribeToNewTodos() {
     client.graphql({ query: onCreateTodo }).subscribe({
@@ -305,12 +165,14 @@ val.then(result => {
     
 }).catch(error => {
     console.error('Errorghjkl;:', error);
+    
 });
 // reloadの基本的な使い方
 function doReload() {
 
     // reloadメソッドによりページをリロード
-    window.location.reload();
+    // window.location.reload();
+    val = fetchCountTodos();
 }
  
 window.addEventListener('load', function () {
@@ -319,54 +181,9 @@ window.addEventListener('load', function () {
     setTimeout(doReload, 60000);
 });
 
-
-// var pieData = [
-// {
-//     value: 100,            // 値
-//     color:"#F7464A",       // 色
-//     highlight: "#FF5A5E",  // マウスが載った際の色
-//     label: "りんご"        // ラベル
-// },
-// {
-//     value: 100,
-//     color: "#41C44E",
-//     highlight: "#6CD173",
-//     label: "メロン"
-// },
-// {
-//     value: 100,
-//     color: "#FDB45C",
-//     highlight: "#FFC870",
-//     label: "みかん"
-// },
-// {
-//     value: 100,
-//     color: "#AA49B8",
-//     highlight: "#C583CF",
-//     label: "ぶどう"
-// },
-// {
-//     value: 30,
-//     color: "#4D5360",
-//     highlight: "#616774",
-//     label: "その他"
-// }
-
-// ];
-
-// window.onload = function () {
-// var ctx = document.getElementById("graph-area").getContext("2d");
-// window.myPie = new Chart(ctx).Pie(pieData);
-// }
-
-
-// alert(toString.call(ls));
-// console.log(toString.call(ls))
-// console.log(`ls is ${ls}`)
-// console.log(val)
 subscribeToNewTodos();
 // fetchCountTodos();
-fetchTodos();
+// fetchTodos();
 
 // import './style.css'
 // import javascriptLogo from './javascript.svg'
