7acd80df0a8f1763be0d956d94fabf3d349b0970
shoichiro-u <uchida.shoichiro.up0@is.naist.jp>
2024-01-18 17:33:48 +0900

    uchida-chenge

---
+++ event_click.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+  data() {
+    return {
+//デフォルトはモーダルウィンドウを閉じる
+    show: false,
+    path: '/Users/shoichiro-u/Desktop/Vue-dustbox/dustbox.gif'
+    }
+  },
+  methods:{
+//モーダルウィンドウを開く要素をクリックしたら
+    open: function(){
+      window.location.href = "https://main.d2v7gdb6u9dfdx.amplifyapp.com/";
+    },
+    open2: function(){
+      this.show = true
+      this.path2 = '/Users/shoichiro-u/Desktop/Vue-dustbox/graph.gif';
+    },
+//モーダルウィンドウを閉じる要素をクリックしたら
+    close: function(){
+      this.show = false
+      this.path = '/Users/shoichiro-u/Desktop/Vue-dustbox/dustbox.gif'
+    },
+  }
+});
+app.mount("#app");
